8622267af325f67647a1dbaecb552762bdff950d
Josep Salvat Lopez <damaskinoss@gmail.com>
2021-05-20 17:03:55 +0200

    Cleanup starter files && eslint + prettier initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // añade la opción "parser" con "babel-eslint"
+  parser: '@typescript-eslint/parser',
+
+  // añade "prettier" (y si quieres "prettier/react") a las opciones de "extends"
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:react/recommended'],
+
+  // y por ultimo, en la llave de "plugins" añade el plugin de eslint para prettier
+  plugins: ['prettier'],
+  rules: {
+    code: 2,
+    useTabs: false,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    indent: 'off',
+    allowTemplateLiterals: 'off',
+    'import/extensions': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
