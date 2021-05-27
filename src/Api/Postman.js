9d7ae7ccf071bc69b26943f4190c577b8435ddaa
Josep Salvat Lopez <damaskinoss@gmail.com>
2021-05-27 15:29:08 +0200

    removed console log

--- src/Api/Postman.js
+++ src/Api/Postman.js
@@ -1,20 +1,16 @@
 export const ApiCall = (data) => {
-  console.log(data);
   const requestOptions = {
     method: 'POST',
     mode: 'cors',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   };
-  console.log(requestOptions);
   const fetchedData = fetch('https://httpbin.org/post', requestOptions)
     .then(async (response) => {
       const isJson = response.headers.get('content-type')?.includes('application/json');
       const data = isJson && (await response.json());
 
-      // check for error response
       if (!response.ok) {
-        // get error message from body or default to response status
         const error = (data && data.message) || response.status;
         return Promise.reject(error);
       }
